058a9527afee51075c1ab328bf91a120fd74f6ac
grillzwitu <grillzwitu@gmail.com>
2022-02-18 18:31:24 -0800

    setup server, api routes and add new user

--- utils/db.js
+++ utils/db.js
@@ -1,33 +1,31 @@
 import { MongoClient } from 'mongodb';
 
 class DBClient {
-  constructor() {
-    this.host = process.env.DB_HOST || 'localhost';
-    this.port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'files_manager';
-    this.client = new MongoClient(`mongodb://${this.host}:${this.port}`, { useUnifiedTopology: true });
-    this.client.connect();
-    this.db = this.client.db(this.database);
-  }
-
-  isAlive() {
-    if (this.client.isConnected()) {
-      return true;
+    constructor() {
+	const HOST = process.env.DB_HOST || 'localhost';
+	const PORT = process.env.DB_PORT || 27017;
+	const DATABASE = process.env.DB_DATABASE || 'files_manager';
+	const url = `mongodb://${HOST}:${PORT}/${DATABASE}`;
+	
+	this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
+	this.client.connect();
     }
-    return false;
-  }
 
-  async nbUsers() {
-    this.db = this.client.db(this.database);
-    const collection = await this.db.collection('users');
-    return collection.countDocuments();
-  }
+    isAlive() {
+	return this.client.isConnected();
+    }
 
-  async nbFiles() {
-    this.db = this.client.db(this.database);
-    const collection = await this.db.collection('files');
-    return collection.countDocuments();
-  }
+    async nbUsers() {
+	const usersDb = this.client.db().collection('users');
+	const numUsers = await usersDb.countDocuments();
+	return numUsers;
+    }
+
+    async nbFiles() {
+	const filesDb = this.client.db().collection('files');
+	const numFiles = await filesDb.countDocuments();
+	return numFiles;
+    }
 }
 
 const dbClient = new DBClient();
